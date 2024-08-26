13ae95960fa0a73c40047a5a6ba1731e8aa8fd3c
leegel <57243461+leegel@users.noreply.github.com>
2024-08-26 09:59:39 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["glint.glintvision.com"];
+pub const PUBLIC_RS_PUB_KEY: &str = "RR+JgsTb2umB5svpw3cJ+Z0xIt6gTY5b8BCyDzBsBY0=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
